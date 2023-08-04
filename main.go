b86bcbc540a6801ceb3d3e7ba192cd0567cb72f8
malakanov <malakanov@alkor.co.ru>
2023-08-04 11:14:23 +0500

    add http server, home handler

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
 
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+
+	log.Println("Server start in on :4000")
+
+	err := http.ListenAndServe("localhost:4000", mux)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+func home(writer http.ResponseWriter, request *http.Request) {
+	_, err := writer.Write([]byte("Hello world"))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
