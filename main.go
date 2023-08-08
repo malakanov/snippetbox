92c440de16c5eb959982ee67b5fc4c129498c787
malakanov <malakanov@alkor.co.ru>
2023-08-08 15:47:23 +0500

    add create, detail handler, not found

--- main.go
+++ main.go
@@ -6,21 +6,43 @@ import (
 )
 
 func main() {
-
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", detail)
+	mux.HandleFunc("/snippet/create", create)
 
 	log.Println("Server start in on :4000")
 
 	err := http.ListenAndServe("localhost:4000", mux)
+
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 }
 
+func create(writer http.ResponseWriter, request *http.Request) {
+	_, err := writer.Write([]byte("Create snippet"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func detail(writer http.ResponseWriter, request *http.Request) {
+	_, err := writer.Write([]byte("Show a snippet"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func home(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Path != "/" {
+		http.NotFound(writer, request)
+		return
+	}
+
 	_, err := writer.Write([]byte("Hello world"))
+
 	if err != nil {
 		log.Fatal(err)
 	}
